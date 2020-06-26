f9b0db17fdab6ee3cc86be393de79a2d2fb64cff
tuist <tuistssip@gmail.com>
2020-06-26 16:25:09 +0500

    tailwind init

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <main></main>
+  <main class="container">
+    <div class="flex justify-between gap-2">
+      <div class="bg-blue-100 text-center flex-1">1</div>
+      <div class="bg-blue-100 text-center flex-1">2</div>
+      <div class="bg-blue-100 text-center flex-1">3</div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
