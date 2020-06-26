bde31ae62a636939d0d2405b3fe63f0bda3dce58
tuist <tuistssip@gmail.com>
2020-06-26 15:17:50 +0500

    initial typescript setup

---
+++ src/views/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <main></main>
+</template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class IndexView extends Vue {}
+</script>
