9e301a49160ebb726ca0996f28e258587065f508
tuist <tuistssip@gmail.com>
2020-09-03 15:12:50 +0500

    init

--- client/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  purge: ['./src/**/*.vue'],
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
-}
--- server/index.js
+++ server/index.js
@@ -4,22 +4,4 @@ const io = require('socket.io')(server)
 
 server.listen(1337)
 
-let count = 0
-
-const cons = (...v) => {
-  console.clear()
-  console.log(...v)
-}
-
-io.on('connection', socket => {
-  if (count === 2) {
-    socket.emit('userOverflow')
-    cons('overflow attempt', count)
-    socket.disconnect()
-    return
-  }
-
-  cons(++count)
-
-  socket.on('disconnect', () => cons(--count))
-})
+io.on('connection', socket => {})
